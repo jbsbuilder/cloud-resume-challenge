e092de4cd4bde0150c900d0a1a7c51d412c91efe
jbsbuilder <work.jacobsmith@gmail.com>
2024-08-26 14:57:20 -0600

    backend added

---
+++ main.tf
@@ -0,0 +1,20 @@
+terraform {
+  backend "s3" {
+    bucket = "jaketerraformstate"
+    key    = "s3/terraform.tfstate""
+    region = var.region
+  }
+}
+
+module "s3" {
+  source           = "./modules/s3-static"
+  bucket_name      = var.bucket_name
+  root_bucket_name = var.root_bucket_name
+}
+
+module "cloudfront" {
+  source         = "./modules/cloudfront"
+  domain_name_cf = module.s3.website_endpoint
+  bucket_name    = var.bucket_name
+  root_domain_cf = module.s3.website_endpoint_root
+}
---
+++ modules/cloudFront/cloudfront/vars.tf
@@ -0,0 +1,3 @@
+variable "domain_name_cf" {}
+variable "bucket_name" {}
+variable "root_domain_cf" {}
