fb0ad34621175498b5abd2f551a540faf7b3216a
jbsbuilder <work.jacobsmith@gmail.com>
2024-09-04 07:54:48 -0600

    almost there

--- modules/s3-static/output.tf
+++ modules/s3-static/output.tf
@@ -2,6 +2,6 @@ output "domain_name" {
   value = aws_s3_bucket.cloud_resume_challenge.bucket_regional_domain_name
 }
 
-ouput "s3_static_bucket_id" {
+output "s3_static_bucket_id" {
   value = aws_s3_bucket.cloud_resume_challenge.id
 }
